c6a928727af9e848da649c32f779596b648a42aa
MUHAMMAD JIBRAN <90435927+Jibran220@users.noreply.github.com>
2022-12-26 11:57:54 +0500

    Update Contacts.jsx

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -23,7 +23,7 @@ export default function Contacts({ contacts, changeChat }) {
         <Container>
           <div className="brand">
             <img src={Logo} alt="logo" />
-            <h3>ROBO aaaaaaaaaaaaaaaaaaaaa</h3>
+            <h3>ROBO CHAT</h3>
           </div>
           <div className="contacts">
             {contacts.map((contact, index) => {
